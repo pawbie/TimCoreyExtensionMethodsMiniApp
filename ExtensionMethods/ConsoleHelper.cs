4a1643edac8c9ab40a0da4722e288b9bf80d310a
Pawel Bielinski <pawbielinski@gmail.com>
2023-09-12 03:56:21 +0200

    Add project files.

---
+++ ExtensionMethods/ConsoleHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+            while (string.IsNullOrWhiteSpace(message))
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static int RequestInteger(this string message)
+        {
+            return message.RequestInteger(false);
+        }
+
+        public static int RequestInteger(this string message, int minValue, int maxValue)
+        {
+            return message.RequestInteger(true, minValue, maxValue);
+        }
+
+        private static int RequestInteger(this string message, bool useRangeValidation, int minValue = 0, int maxValue = 0)
+        {
+            int output = 0;
+            bool isValidInteger = false;
+            bool isInRange = true;
+
+            while (isValidInteger == false && isInRange == false)
+            {
+                Console.Write(message);
+                isValidInteger = int.TryParse(message, out output);
+                if (useRangeValidation == true) isInRange = output.IsInRange(minValue, maxValue);
+            }
+
+            return output;
+        }
+
+        private static bool IsInRange(this int number, int minValue, int maxValue)
+        {
+            return number >= minValue && number <= maxValue;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new PersonModel();
+
+            person.FirstName = "Please provide first name: ".RequestString();
+            person.LastName = "Please provide last name: ".RequestString();
+            person.Age = "Please provide age: ".RequestInteger(0, 120);
+
+            Console.ReadLine();
+        }
+    }
+}
